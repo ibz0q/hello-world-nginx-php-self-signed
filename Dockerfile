6138f5511bd0737dff026d48d9ba58540a976be4
ibz0q <ibz@ibz.com>
2024-11-04 13:59:23 +0000

    clean up files

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY nginx.conf /etc/nginx/nginx.conf
 COPY index.html /usr/share/nginx/html/index.html
 
 COPY entrypoint.sh /entrypoint.sh
+
 RUN chmod +x /entrypoint.sh
 
 EXPOSE 80 443
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Create directories for SSL certificates if they don't exist
 mkdir -p /etc/ssl/certs /etc/ssl/private
 
-# Generate a self-signed SSL certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/nginx-selfsigned.key \
     -out /etc/ssl/certs/nginx-selfsigned.crt \
     -subj "/CN=localhost"
 
-# Start Nginx
 nginx -g "daemon off;"
